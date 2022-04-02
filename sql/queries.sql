da22b9a8be8801d7868db8298793796a4011e80a
SuhabeUIC <98372830+SuhabeUIC@users.noreply.github.com>
2022-04-01 20:28:12 -0500

    Update queries.sql

--- sql/queries.sql
+++ sql/queries.sql
@@ -14,3 +14,23 @@ CREATE VIEW hannah_complex AS
     FROM employee
     INNER JOIN room
     ON employee.employee_id = room.employee_id;
+    
+CREATE VIEW suhabe_simple AS
+    	SELECT patient_id
+    FROM patient
+    WHERE age > '70'
+    ORDER BY age;
+    
+CREATE VIEW suhabe_aggregate AS
+    	SELECT COUNT(hospitalization_date)
+    FROM patient
+    WHERE hospitalization_date > '12/04/2021 12:00:00 AM'
+    ORDER BY hospitalization_date;
+    
+CREATE VIEW suhabe_complex AS
+    	SELECT patient_id
+    FROM patient
+    WHERE EXISTS (
+        	SELECT condition_id, condition_name
+        FROM patient_preexisting_conditions
+        WHERE condition_name = 'diabetes');
