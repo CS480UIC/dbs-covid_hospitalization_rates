6b03247b8b3a6d95b9ec2082328412b9a73a67c4
SuhabeUIC <98372830+SuhabeUIC@users.noreply.github.com>
2022-04-26 19:23:10 -0500

    Update deleteDB.sql

--- sql/deleteDB.sql
+++ sql/deleteDB.sql
@@ -2,6 +2,6 @@ DROP TABLE employee;
 DROP TABLE patient;
 DROP TABLE country;
 DROP TABLE room;
-DROP TABLE patient_preexisting_conditions;
+DROP TABLE patient_conditions;
 DROP TABLE hospital;
 DROP TABLE bill;
